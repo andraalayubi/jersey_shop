0223daa82c1b96c31dc5598db8202a14c04e7d85
andraalayubi <andraalayubi@gmail.com>
2024-06-04 01:50:00 +0700

    build react

---
+++ public/build/assets/Container-DTDRl9A1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-CKkwK5Co.js";const n=({children:s})=>t.jsx("div",{className:"px-6 md:px-12 lg:px-20 py-7",children:s}),r=n;export{r as C};
---
+++ public/build/assets/DirectionProvider-BUZmjoiZ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-CKkwK5Co.js";const e=t.createContext({dir:"ltr",toggleDirection:()=>{},setDirection:()=>{}});function o(){return t.useContext(e)}export{o as u};
